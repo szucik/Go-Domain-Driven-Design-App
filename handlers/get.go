fa9a630aee2e59d6eafc4f8cb8764960bc970857
Marcin Szut <marcin.szut@gmail.com>
2021-10-25 14:27:33 +0200

    Add Swagger code documentation and Cors handler

---
+++ handlers/get.go
@@ -0,0 +1,22 @@
+package handlers
+
+import (
+	"github.com/zlyjoker102/simple-rest-api/data"
+	"net/http"
+)
+
+// swagger:route GET /users users listUsers
+// Return a list of users from the database
+// responses:
+//	200: usersResponse
+
+// GetUsers returns Users from data store
+func (u *Users) GetUsers(rw http.ResponseWriter, r *http.Request) {
+	u.l.Println("Handle GET Users")
+	rw.Header().Add("Content-Type", "application/json")
+	lu := data.GetUsers()
+	err := lu.ToJSON(rw)
+	if err != nil {
+		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+	}
+}
